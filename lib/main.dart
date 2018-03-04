c09026c16e23fa77a2e1931576ad19c8c07d45d8
Emmanuel Proulx <emmanuel.proulx.1@ens.etsmtl.ca>
2018-03-04 10:05:42 -0500

    Fixed second image bug

--- lib/main.dart
+++ lib/main.dart
@@ -367,7 +367,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.of(context).push(
       new MaterialPageRoute(
         builder: (context) {
-
+          memoryImage = new FileImage(guide.imageFile);
           return new Scaffold(
             appBar: new AppBar(
               title: new Text(guide.name),
